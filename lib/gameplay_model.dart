edcd907b83aff94b43682b60ea9565476b65a3db
Karol Onyśko <karol.onysko@cic.com>
2024-12-27 19:45:14 +0100

    Added core of the game

---
+++ lib/gameplay_model.dart
@@ -0,0 +1,122 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+sealed class GameplayState {}
+
+final class Initial extends GameplayState {}
+
+final class InProgress extends GameplayState {}
+
+final class Overlayed extends GameplayState {
+  final Color color;
+
+  Overlayed(this.color);
+}
+
+final class Paused extends GameplayState {}
+
+final class Completed extends GameplayState {}
+
+class WordResult {
+  final String word;
+  final bool isGuessed;
+
+  WordResult(this.word, this.isGuessed);
+}
+
+class GameplayModel extends ChangeNotifier {
+  static const int _initialTime = 60;
+  final List<String> _words = [
+    'Dog',
+    'Cat',
+    'Elephant',
+    'Lion',
+    'Tiger',
+    'Bear',
+    'Wolf',
+  ];
+  GameplayState gameplayState = Initial();
+
+  Timer? _timer;
+  int _timeLeft = _initialTime;
+  String _currentWord = "";
+
+  final List<WordResult> guessedWords = [];
+
+  int get timeLeft => _timeLeft;
+
+  String get currentWord => _currentWord;
+
+  bool get isTimeOver => _timeLeft <= 0;
+
+  // Start/resume the game
+  void startGame() {
+    // If there is an active timer, cancel it to avoid duplication
+    _timer?.cancel();
+
+    // Reset time if needed
+    _timeLeft = _initialTime;
+    guessedWords.clear();
+    _currentWord = _getRandomWord()!;
+    gameplayState = InProgress();
+    notifyListeners();
+
+    // Start countdown
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_timeLeft > 0) {
+        _timeLeft--;
+      } else {
+        gameplayState = Completed();
+        timer.cancel();
+      }
+      notifyListeners();
+    });
+  }
+
+  // Choose next word (helper method)
+  String? _getRandomWord() {
+    if (_words.isEmpty) return null;
+    _words.shuffle();
+    return _words.removeAt(0);
+  }
+
+  // When user presses skip
+  void skipWord() {
+    if (!isTimeOver) {
+      guessedWords.add(WordResult(_currentWord, false));
+      showOverlay(Colors.green);
+    }
+  }
+
+  // When user presses guessed
+  void guessedWord() {
+    if (!isTimeOver) {
+      guessedWords.add(WordResult(_currentWord, true));
+      showOverlay(Colors.green);
+    }
+  }
+
+  void showOverlay(Color color) {
+    gameplayState = Overlayed(color);
+    notifyListeners();
+    Future.delayed(Duration(seconds: 1), () {
+      gameplayState = InProgress();
+      final word = _getRandomWord();
+      if (word != null) {
+        _currentWord = word;
+      } else {
+        _timer?.cancel();
+        gameplayState = Completed();
+      }
+      notifyListeners();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+}
---
+++ lib/gameplay_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'gameplay_model.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      left: false,
+      bottom: false,
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple[50],
+        body: Stack(
+          children: [
+            Consumer<GameplayModel>(
+              builder: (context, gameProvider, child) {
+                if (gameProvider.gameplayState is Completed) {
+                  return _buildResults(gameProvider);
+                }
+                return _buildLandscapeLayout(context, gameProvider);
+              },
+            ),
+            Consumer<GameplayModel>(
+              builder: (context, gameProvider, child) {
+                if (gameProvider.gameplayState is Overlayed) {
+                  return Container(
+                    color: (gameProvider.gameplayState as Overlayed)
+                        .color
+                        .withOpacity(0.8),
+                  );
+                }
+                return SizedBox.shrink();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLandscapeLayout(BuildContext context, GameplayModel provider) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Skip button
+          ElevatedButton(
+            onPressed: provider.skipWord,
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.close,
+                  size: 64,
+                  color: Colors.white,
+                ),
+                Text('Skip',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white))
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  provider.currentWord,
+                  style: const TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurpleAccent),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Time left: ${provider.timeLeft} s',
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+
+          // Guessed button
+          ElevatedButton(
+            onPressed: provider.guessedWord,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.check,
+                  size: 64,
+                  color: Colors.white,
+                ),
+                Text('Guess',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildResults(GameplayModel provider) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Game is over',
+          style: TextStyle(
+              fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        const SizedBox(height: 20),
+        Expanded(
+          child: Center(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: provider.guessedWords.length,
+              itemBuilder: (context, index) {
+                final wordResult = provider.guessedWords[index];
+                return ListTile(
+                  title: Text(
+                    wordResult.word,
+                    style: TextStyle(
+                        fontSize: 24,
+                        color:
+                            wordResult.isGuessed ? Colors.green : Colors.red),
+                  ),
+                  leading: Icon(
+                    wordResult.isGuessed ? Icons.check : Icons.close,
+                    color: wordResult.isGuessed ? Colors.green : Colors.red,
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
